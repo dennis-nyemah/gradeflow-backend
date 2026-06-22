d07a4cef4ca0c7890024c87df22fdfbc223cce11
dennis <nyemahdennis1@gmail.com>
2026-06-22 07:20:23 -0700

    Add project snapshots to README

--- src/main/java/com/gradeflow/entity/Grade.java
+++ src/main/java/com/gradeflow/entity/Grade.java
@@ -1,11 +1,23 @@
 package com.gradeflow.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapKeyColumn;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Map;
 
@@ -25,7 +37,8 @@ import java.util.Map;
  */
 @Entity
 @Table(name = "grades")
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
